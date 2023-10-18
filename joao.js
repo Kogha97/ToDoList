c564cf2c971729a7edc1d2c0d9e890c85bc035ed
Kogha97 <poiuytrewq7991@gmail.com>
2023-10-18 19:28:15 +0100

    edit button added

--- joao.js
+++ joao.js
@@ -22,12 +22,88 @@ function handleClick() {
   const li = document.createElement('li');
   const span = document.createElement('span');
   span.textContent = inputValue;
-//delete icon
+//delete icon variables
   const icon = document.createElement('i');
-  icon.classList = 'fa-solid fa-x';
+  icon.classList = 'fa-solid fa-trash';
+//edit icon variables
+  const iconEdit = document.createElement('i');
+  iconEdit.classList = 'fa-solid fa-pen'
+  
+  const iconSpanDiv = document.createElement('div')
+  iconSpanDiv.classList.add('iconText')
+//append edit icon + text
+  li.appendChild(iconSpanDiv);
+  iconSpanDiv.appendChild(iconEdit);
+  iconSpanDiv.appendChild(span);
+ //apopend delete button
+  li.appendChild(icon)
 
-  li.appendChild(span);
-  li.appendChild(icon);
+// edit button 
+/*
+ let isEditing = false;
+  iconEdit.addEventListener('click', () =>{
+    if(isEditing){
+      return;
+    }
+  span.contentEditable = true;
+  span.focus()
+  isEditing = true;
+
+  });
+  span.addEventListener('keydown', (event)=>{
+    if(event.key ==='Enter'){
+      span.contentEditable = false;
+      isEditing = false;
+    }
+  })
+
+  span.addEventListener('blur', () =>{
+    span.contentEditable = false;
+    isEditing = false;
+  })
+
+  li.addEventListener('click', () =>{
+    if(!isEditing){
+      li.classList.toggle('checked');
+    }
+    });
+  */
+ // ...
+
+let isEditing = false;
+
+iconEdit.addEventListener('click', () => {
+  if (isEditing) {
+    return;
+  }
+  span.contentEditable = true;
+  span.focus();
+  input.classList.add('editing');
+  isEditing = true;
+});
+
+span.addEventListener('click', () => {
+  if (!isEditing) {
+    li.classList.toggle('checked');
+  }
+});
+
+span.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    span.contentEditable = false;
+    isEditing = false;
+    input.classList.remove('editing');
+  }
+  });
+
+span.addEventListener('blur', () => {
+  span.contentEditable = false;
+  isEditing = false;
+  input.classList.remove('editing');
+});
+
+
+
 //condition to insert the deadline input into the right table
   if (selectedDeadline === 'today') {
     todayList.appendChild(li);
@@ -42,8 +118,46 @@ function handleClick() {
   });
 //dashed text on click
   li.addEventListener('click', () => {
-    li.classList.toggle('checked');
+    span.classList.toggle('checked');
   });
 
+
+
   input.value = '';
+
+
 }
+
+
+/* W Functionality to add
+
+EDIT BUTTON
+
+add edit button  next to do delete -> on click allows to edit the list item.
+
+
+1) create an edit icon when list is created (DONE)
+
+2) create a function to click the edit button => on that function make it edit the text
+
+      when click the button, we get a new input window where the text was, with the current text
+      so the user can change it
+
+
+FINISHED TASK
+
+when checked -> add button -> button moves to finished list
+
+1) create icon when checked is clicked
+
+2) create function for when icon is clicked the item is moved to finished list
+
+possible idea for finished task
+
+  if( 'checked' === true){
+    moveButton();
+  }
+
+Possible work on buttons, to make the priority / deadline appear has priority / deadline
+
+*/
